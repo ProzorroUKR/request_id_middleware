3efb738272d19a7500b6d2b4607e485310c8ab9a
Taras Kozlovskyi <ktarasz@quintagroup.com>
2015-02-03 14:25:25 +0200

    Added handle X-Client-Request-ID

--- request_id_middleware/middleware.py
+++ request_id_middleware/middleware.py
@@ -11,20 +11,29 @@ class RequestIdMiddleware(base.Middleware):
 
     [filter:request_id]
     paste.filter_factory = request_id_middleware.middleware:RequestIdMiddleware.factory
+    env_client_request_id = custom_env_client_request_id
+    resp_header_client_request_id = custom-x-client-request-id
     env_request_id = custom_env_request_id
     resp_header_request_id = custom-x-request-id
     """
     @classmethod
-    def factory(cls, global_conf, 
+    def factory(cls, global_conf,
+                env_client_request_id='x_client_request_id',
+                resp_header_client_request_id='X-Client-Request-ID',
                 env_request_id='x_request_id',
-                resp_header_request_id = 'X-Request-ID'):
-
+                resp_header_request_id='X-Request-ID'):
+        cls.env_client_request_id = env_client_request_id
+        cls.resp_header_client_request_id = resp_header_client_request_id
         cls.env_request_id = env_request_id
         cls.resp_header_request_id = resp_header_request_id
         return cls
 
     @webob.dec.wsgify
     def __call__(self, req):
+        client_request_id = None
+        if self.env_client_request_id in req.headers:
+            client_request_id = req.headers[self.resp_header_client_request_id]
+            req.environ[self.env_client_request_id] = client_request_id
         if self.resp_header_request_id in req.headers:
             req_id = req.headers[self.resp_header_request_id]
         else:
@@ -32,6 +41,9 @@ class RequestIdMiddleware(base.Middleware):
 
         req.environ[self.env_request_id] = req_id
         response = req.get_response(self.application)
+        if client_request_id:
+            response.headers.add(self.resp_header_client_request_id, client_request_id)
         if self.resp_header_request_id not in response.headers:
             response.headers.add(self.resp_header_request_id, req_id)
+
         return response
